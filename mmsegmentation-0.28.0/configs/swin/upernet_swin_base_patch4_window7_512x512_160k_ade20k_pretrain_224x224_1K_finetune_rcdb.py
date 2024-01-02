a60bbe5b5ec588249f33d37be07aa7072ca06f21
Risa Shinoda <risashinoda@Risas-MacBook-Pro.local>
2024-01-02 23:10:52 +0900

    add mmsegmentation

---
+++ mmsegmentation-0.28.0/configs/swin/upernet_swin_base_patch4_window7_512x512_160k_ade20k_pretrain_224x224_1K_finetune_rcdb.py
@@ -0,0 +1,18 @@
+_base_ = [
+    './upernet_swin_tiny_patch4_window7_512x512_160k_ade20k_'
+    'pretrain_224x224_1K_finetune_rcdb.py'
+]
+
+checkpoint_file ="put_your_checkpoint_path"
+model = dict(
+    backbone=dict(
+        init_cfg=dict(type='Pretrained', checkpoint=checkpoint_file),
+        #_delete_=True,
+        # init_cfg=None,
+        embed_dims=128,
+        depths=[2, 2, 18, 2],
+        num_heads=[4, 8, 16, 32],
+        ),
+    decode_head=dict(in_channels=[128, 256, 512, 1024], num_classes=150),
+    auxiliary_head=dict(in_channels=512, num_classes=150))
+
---
+++ mmsegmentation-0.28.0/configs/swin/upernet_swin_tiny_patch4_window7_512x512_160k_ade20k_pretrain_224x224_1K_pretrain_rcdb.py
@@ -0,0 +1,49 @@
+_base_ = [
+    '../_base_/models/upernet_swin_pretrain_rcdb.py', '../_base_/datasets/rcdb.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_300.py'
+]
+
+#checkpoint_file = "/groups/gae50924/shinoda/mmsegmentation/data/convert_swin_base_patch4_window7_224_imagenet1k_300ep.pth" # noqa
+model = dict(
+    backbone=dict(
+        #init_cfg=dict(type='Pretrained', checkpoint=checkpoint_file),
+        init_cfg=None,
+        #frozen_stages=4,
+        embed_dims=96,
+        depths=[2, 2, 6, 2],
+        num_heads=[3, 6, 12, 24],
+        window_size=7,
+        use_abs_pos_embed=False,
+        drop_path_rate=0.3,
+        patch_norm=True),
+    decode_head=dict(in_channels=[96, 192, 384, 768], num_classes=255),
+    auxiliary_head=dict(in_channels=384, num_classes=255))
+
+# AdamW optimizer, no weight decay for position embedding & layer norm
+# in backbone
+optimizer = dict(
+    _delete_=True,
+    type='AdamW',
+    lr=0.0006,
+    betas=(0.9, 0.999),
+    weight_decay=0.01,
+    paramwise_cfg=dict(
+        custom_keys={
+            'absolute_pos_embed': dict(decay_mult=0.),
+            'relative_position_bias_table': dict(decay_mult=0.),
+            'norm': dict(decay_mult=0.),
+            #'head': dict(lr_mult=10.)
+        }))
+
+lr_config = dict(
+    _delete_=True,
+    policy='poly',
+    warmup='linear',
+    warmup_iters=1500,
+    warmup_ratio=1e-6,
+    power=1.0,
+    min_lr=0.0,
+    by_epoch=False)
+
+# By default, models are trained on 8 GPUs with 2 images per GPU
+data = dict(samples_per_gpu=4)
